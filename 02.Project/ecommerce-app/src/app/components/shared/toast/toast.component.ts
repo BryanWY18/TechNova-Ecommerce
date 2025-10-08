e0de908fba53fb200df2a1f78bd1bb8e913fd2bf
Darktortilla <elgar-frank@hotmail.com>
2025-10-07 21:29:51 -0600

    toast component con historial, mensajes de error y success

--- 02.Project/ecommerce-app/src/app/components/shared/toast/toast.component.ts
+++ 02.Project/ecommerce-app/src/app/components/shared/toast/toast.component.ts
@@ -2,12 +2,13 @@ import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core'
 import { ToastService } from '../../../core/services/toast/toast.service';
 import { Observable, of, scan, Subscription, takeUntil } from 'rxjs';
 import { ToastMessage } from '../../../core/types/ToastMessage';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule, NgClass } from '@angular/common';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-toast',
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, NgClass],
+  // imports: [CommonModule],
   standalone: true,
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.css',
