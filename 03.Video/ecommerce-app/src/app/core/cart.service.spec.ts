3c183464b1623c248eb96ba09179380d973f02c8
Rodrigo Leaños Bermejo <rodrigoBermejo@users.noreply.github.com>
2025-10-20 12:43:53 -0600

    Add cart service and related tests
    Introduced CartService and its unit tests to core module. Removed unused AuthGuard spec. Updated app.component.spec.ts and package.json for new dependencies.

---
+++ 03.Video/ecommerce-app/src/app/core/cart.service.spec.ts
@@ -0,0 +1,29 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { CartService } from './cart.service';
+
+describe('CartService', () => {
+  let service: CartService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [CartService],
+    });
+
+    service = TestBed.inject(CartService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
