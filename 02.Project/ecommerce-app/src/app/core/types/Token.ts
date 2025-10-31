7d887e4cf7de87445c3813a783c36262c0d87d2b
Darktortilla <elgar-frank@hotmail.com>
2025-10-30 20:48:44 -0600

    ngrx install

--- 02.Project/ecommerce-app/src/app/core/types/Token.ts
+++ 02.Project/ecommerce-app/src/app/core/types/Token.ts
@@ -6,3 +6,10 @@ export const tokenSchema = z.object({
 }); 
 
 export type token = z.infer<typeof tokenSchema>; 
+
+
+export type decodedToken = {
+  userId: string;
+  displayName: string;
+  role: 'admin' | 'customer' | 'guest';
+};
