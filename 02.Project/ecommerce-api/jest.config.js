9f52b14b624b33754dc22b9acfc4d4dcf0a471d6
Darktortilla <elgar-frank@hotmail.com>
2025-11-11 20:38:08 -0600

    add test in auth.guard

--- 02.Project/ecommerce-api/jest.config.js
+++ 02.Project/ecommerce-api/jest.config.js
@@ -1,6 +1,6 @@
 export default {
   testEnvironment: "node", // en entorno en donde trabajamos
-  transform: {}, //import/exporte en cuaalquier lugar
+  transform: {}, //import/export en cualquier lugar
   moduleFileExtensions: ["js", "json"], //los archivos que queremos probar
   testMatch: ["**/__test__/**/*.test.js", "**/?(*.)+(spec|test).js"], //donde se encuentran nuestras pruebas
   testPathIgnorePatterns: ["/node_modules/"], //carpetas donde no queremos hacer pruebas
--- 02.Project/ecommerce-app/src/app/core/guards/auth/auth.guard.spec.ts
+++ 02.Project/ecommerce-app/src/app/core/guards/auth/auth.guard.spec.ts
@@ -1,17 +1,58 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
-
+import { CanActivateFn, Router } from '@angular/router';
 import { authGuard } from './auth.guard';
+import { selectIsAuthenticated } from '../../store/auth/auth.selectors';
+import {provideMockStore, MockStore} from '@ngrx/store/testing'
 
 describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  let guard: CanActivateFn;
+  let store: MockStore;
+  let router: jasmine.SpyObj<Router>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
+  beforeEach(()=>{
+    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({
+          selectors: [
+            {selector: selectIsAuthenticated, value: false},
+          ]
+        }),
+        {provide: Router, useValue: routerSpy}
+      ]
+    });
+
+    store = TestBed.inject(MockStore);
+    router = TestBed.inject(Router) as jasmine.SpyObj<Router>;
+
+    guard = (...guardParametres) => TestBed.runInInjectionContext(()=>authGuard(...guardParametres));
+  })
+  it('deberia ser creado', ()=>{
+    // let name = undefined;
+    // expect(name).toBeTruthy() no pasaria la prueba por que el nombre no esta definido;
+    expect(guard).toBeTruthy();
   });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+  it('debería permitir el acceso cuando el usuario está autenticado',()=>{
+    store.overrideSelector(selectIsAuthenticated, true);
+    store.refreshState();
+
+    const result = guard(null as any, null as any);
+
+    expect(result).toBeTrue();
+    expect(router.navigateByUrl).not.toHaveBeenCalled();
   });
+
+  it('no debería permitir el acceso si el usuario no está atenticado', ()=>{
+    store.overrideSelector(selectIsAuthenticated, false);
+    store.refreshState();
+
+    const result = guard(null as any, null as any);
+    
+    expect(result).toBe(false);
+    expect(router.navigateByUrl).toHaveBeenCalled();
+  })
+
+
 });
