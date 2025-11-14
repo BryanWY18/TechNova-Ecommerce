3c50913bd82cfa42820f654537e48e0e94eab9ee
Darktortilla <elgar-frank@hotmail.com>
2025-11-13 21:44:59 -0600

    login and add to cart cypress test, create payment method

--- 02.Project/ecommerce-app/src/app/core/services/paymentMethods/payment-methods.service.ts
+++ 02.Project/ecommerce-app/src/app/core/services/paymentMethods/payment-methods.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable, switchMap, take } from 'rxjs';
+import { BehaviorSubject, map, Observable, switchMap, take, tap } from 'rxjs';
 import {
   CreatePaymentMethod,
   PaymentMethod,
   PaymentMethodArraySchema,
+  UpdatePaymentMethod,
 } from '../../types/PaymentMethod';
 import { Store } from '@ngrx/store';
 import { selectUserId } from '../../store/auth/auth.selectors';
@@ -46,9 +47,31 @@ export class PaymentMethodsService {
       user,
       ...data,
     };
+    return this.http.post(this.baseUrl, payload).pipe(
+      switchMap(
+        () => this.getPaymentMethodsByUser(user)),
+        tap((updatedData) => {
+          this.paymentSubject.next(updatedData);
+        })
+      
+    );
+  }
 
+  // 1,3,4,5,6,7,8,9,0
+  updatePaymentMethod(
+    updatedPaymentmethodData: UpdatePaymentMethod
+  ): Observable<PaymentMethod[]> {
+    const userId = this.getUserId();
     return this.http
-      .post(this.baseUrl, payload)
-      .pipe(switchMap(() => this.getPaymentMethodsByUser(user)));
+      .put(
+        `${this.baseUrl}/${updatedPaymentmethodData._id}`,
+        updatedPaymentmethodData
+      )
+      .pipe(
+        switchMap(() => this.getPaymentMethodsByUser(userId)),
+        tap((updatedData) => {
+          this.paymentSubject.next(updatedData);
+        })
+      );
   }
 }
